0d66e12d80755965d64cb2e15610f273191b91f9
tfbc <tfbc.jason@gmail.com>
2015-07-07 15:42:16 +1000

    Basic mod file, project files added.

---
+++ src/main/java/com/tfbc/dtech/DTech.java
@@ -0,0 +1,33 @@
+package com.tfbc.dtech;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "dtech", name="DTech", version = "1.8-1.0")
+public class DTech 
+{
+	
+	@Mod.Instance("DTech")
+	public static DTech instance;
+	
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		
+	}
+	
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		
+	}
+	
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		
+	}
+}
